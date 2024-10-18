918a55ca56cb3055f9dec61c0b9c31a6ea38441d
Nicholas Yeung <njyeung@wisc.edu>
2024-10-18 12:06:07 -0500

    partly working drag

--- src/app/projects/card/card.component.ts
+++ src/app/projects/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -8,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrl: './card.component.css'
 })
 export class CardComponent {
+  @ViewChild('card') container? : ElementRef;
+  
+  ngAfterViewInit() {
+    // var card = this.container?.nativeElement
+    // card.style.top = '50%'
+    // card.style.left = '50%'
 
+    // var drag: any = null;
+    
+    // card.onmousedown = function() {
+    //   drag = card
+    // }
+    // document.onmouseup = function() {
+    //   drag = null
+    // }
+    // document.onmousemove = function(e) {
+    //   if (drag == null) return
+      
+    //   var x = e.pageX;
+    //   var y = e.pageY
+
+    //   card.style.top = y + 'px'
+    //   card.style.left = x + 'px'
+    //   // console.log(x + ' ' + y)
+    // }
+  }
 }
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { CardComponent } from './card/card.component';
 
 @Component({
@@ -10,4 +10,53 @@ import { CardComponent } from './card/card.component';
 })
 export class ProjectsComponent {
   deskSrc : string = 'assets/desk/desk-light.jpg'
+
+  @ViewChildren(CardComponent, {read: ElementRef}) cards!: QueryList<ElementRef>;
+  @ViewChild('plane') plane? : ElementRef;
+  @ViewChild('detector') detector? : ElementRef;
+
+  ngAfterViewInit() {
+    
+    
+    const rect = this.plane?.nativeElement.getBoundingClientRect()
+    this.cards.forEach((element)=> {
+      var card = element.nativeElement;
+
+      card.style.top = '50%'
+      card.style.left = '50%'
+
+      var drag: any = null;
+      let offsetX = 0;
+      let offsetY = 0; 
+      
+      card.onmousedown = function(e:any) {
+        drag = card
+        const rect = card.getBoundingClientRect();
+        console.log(rect)
+        offsetX = e.clientX - rect.left;
+        offsetY = e.clientY - rect.top;
+      }
+      document.onmouseup = function() {
+        drag = null
+      }
+      document.onmousemove = function(e) {
+        
+        if (drag == null) return
+        const mappedX = ((e.clientX - rect.left - offsetX) / rect.width) * 100;
+        const mappedY = ((e.clientY - rect.top - offsetY) / rect.height) * 100;
+
+        // Set the card's position using the calculated percentages
+        drag.style.position = 'absolute'; // Ensure absolute positioning
+        drag.style.left = mappedX + '%';
+        drag.style.top = mappedY + '%';
+
+        // var x = e.clientX
+        // var y = e.clientY
+        // card.style.top = y + 'px'
+        // card.style.left = x + 'px'
+        // console.log(x + ' ' + y)
+      }
+    })
+  }
+
 }
