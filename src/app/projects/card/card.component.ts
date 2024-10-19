e2770dbb5322f6d9fd90fd3caf2f68afb57ea4e9
Nicholas Yeung <njyeung@wisc.edu>
2024-10-18 22:52:32 -0500

    message

--- src/app/projects/card/card.component.ts
+++ src/app/projects/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -8,30 +8,7 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
   styleUrl: './card.component.css'
 })
 export class CardComponent {
-  @ViewChild('card') container? : ElementRef;
-  
-  ngAfterViewInit() {
-    // var card = this.container?.nativeElement
-    // card.style.top = '50%'
-    // card.style.left = '50%'
+  @ViewChild('frame') frame? : ElementRef
+  @Input() hideiframe: boolean = false;
 
-    // var drag: any = null;
-    
-    // card.onmousedown = function() {
-    //   drag = card
-    // }
-    // document.onmouseup = function() {
-    //   drag = null
-    // }
-    // document.onmousemove = function(e) {
-    //   if (drag == null) return
-      
-    //   var x = e.pageX;
-    //   var y = e.pageY
-
-    //   card.style.top = y + 'px'
-    //   card.style.left = x + 'px'
-    //   // console.log(x + ' ' + y)
-    // }
-  }
 }
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { asNativeElements, Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { CardComponent } from './card/card.component';
 
 @Component({
@@ -10,53 +10,136 @@ import { CardComponent } from './card/card.component';
 })
 export class ProjectsComponent {
   deskSrc : string = 'assets/desk/desk-light.jpg'
-
+  deskLayerSrc : string = 'assets/desk/desk-light-layer.png'
   @ViewChildren(CardComponent, {read: ElementRef}) cards!: QueryList<ElementRef>;
   @ViewChild('plane') plane? : ElementRef;
-  @ViewChild('detector') detector? : ElementRef;
 
+  hideiframe:boolean = false;
+  
   ngAfterViewInit() {
-    
-    
-    const rect = this.plane?.nativeElement.getBoundingClientRect()
+    var initialTop = '40%';
+    var initialLeft = '50%';
+    var zIndexList : any = []
+
     this.cards.forEach((element)=> {
       var card = element.nativeElement;
+     
+      // z index management
+      card.style.zIndex = String(zIndexList.indexOf(card) + 10);
+      zIndexList.push(card)
 
-      card.style.top = '50%'
-      card.style.left = '50%'
-
-      var drag: any = null;
-      let offsetX = 0;
-      let offsetY = 0; 
+      // card spawns
+      card.style.top = initialTop;
+      card.style.left = initialLeft;
+      initialTop = `${Number(initialTop.split("%")[0]) - 0.5}%`
+      initialLeft = `${(Number(initialLeft.split("%")[0]) + 0.2)}%`
       
-      card.onmousedown = function(e:any) {
-        drag = card
-        const rect = card.getBoundingClientRect();
-        console.log(rect)
-        offsetX = e.clientX - rect.left;
-        offsetY = e.clientY - rect.top;
-      }
-      document.onmouseup = function() {
-        drag = null
-      }
-      document.onmousemove = function(e) {
-        
-        if (drag == null) return
-        const mappedX = ((e.clientX - rect.left - offsetX) / rect.width) * 100;
-        const mappedY = ((e.clientY - rect.top - offsetY) / rect.height) * 100;
+      let isDragging = false;
 
-        // Set the card's position using the calculated percentages
-        drag.style.position = 'absolute'; // Ensure absolute positioning
-        drag.style.left = mappedX + '%';
-        drag.style.top = mappedY + '%';
+      let offsetX = 0;
+      let offsetY = 0;
 
-        // var x = e.clientX
-        // var y = e.clientY
-        // card.style.top = y + 'px'
-        // card.style.left = x + 'px'
-        // console.log(x + ' ' + y)
+      let prev = card.getBoundingClientRect();
+
+      card.addEventListener('mousedown', (event: MouseEvent) => {
+        event.preventDefault();
+        this.hideiframe = true;
+
+        // z index shift
+        zIndexList.splice(zIndexList.indexOf(card),1)
+        zIndexList.push(card)
+        for(let i=0;i<zIndexList.length;i++) {
+          zIndexList[i].style.zIndex = String(i + 10)
+        }
+
+        isDragging = true;
+        prev = card.getBoundingClientRect();
+        offsetX = event.clientX - card.getBoundingClientRect().left;
+        offsetY = event.clientY - card.getBoundingClientRect().top;
+        card.style.cursor = 'grabbing';
+      });
+
+      document.addEventListener('mousemove', (event: MouseEvent) => {
+      if (isDragging == true) {
+        this.moveCard(card,event.clientX, event.clientY, offsetX, offsetY);
       }
-    })
+    });
+
+      document.addEventListener('mouseup', () => {
+        if(isDragging == true) {
+          if(card.getBoundingClientRect().x == prev.x && card.getBoundingClientRect().y == prev.y) {
+            console.log("CLICK")
+          }
+        }
+        isDragging = false;
+        card.style.cursor = 'grab';
+      });
+    });
+  }
+  
+  moveCard(card: any, mouseX: number, mouseY: number, offsetX: number, offsetY: number) {
+    const { x, y } = this.getTransformedCoordinates(mouseX, mouseY);
+    
+
+    console.log(window.innerWidth/2, window.innerHeight/4)
+    console.log(x, y)
+
+    // boundary calculation
+    var left = x/2560<2/5
+    var top = y/1440<1/6
+    if(left && top) {
+      
+    }
+    else if(!left && top) {
+      card.style.left = `${(x - offsetX)}px`;
+    }
+    else if(left && !top) {
+      card.style.top = `${(y - offsetY)}px`;
+    }
+    else {
+      card.style.left = `${(x - offsetX)}px`;
+      card.style.top = `${(y - offsetY)}px`;
+    }
+    
+  }
+
+  getTransformedCoordinates(mouseX: number, mouseY: number) {
+    const planeElement = this.plane?.nativeElement
+    const rect = planeElement.getBoundingClientRect();
+
+    const matrix:any = window.getComputedStyle(planeElement).transform;
+
+    if (matrix === 'none') {
+      return { x: mouseX - rect.left, y: mouseY - rect.top };
+    }
+    
+    const matrixValues = matrix.match(/matrix.*\((.+)\)/)[1].split(', ').map(Number);
+
+    // ????????????????????
+
+    // const [a, b, c, d, e, f] = matrixValues
+    
+    // // Calculate the inverse transformation
+    // const determinant = a * d - b * c;
+    // const invA = d / determinant;
+    // const invB = -b / determinant;
+    // const invC = -c / determinant;
+    // const invD = a / determinant;
+    // const invE = (c * f - e * d) / determinant;
+    // const invF = (b * e - a * f) / determinant;
+  
+    // const localX = invA * (mouseX - rect.left) + invC * (mouseY - rect.top) + invE;
+    // const localY = invB * (mouseX - rect.left) + invD * (mouseY - rect.top) + invF;
+
+    const [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p] = matrixValues;
+
+    // I HAVE NO CLUE HOW THIS WORKS BUT IT DOES
+    const localX = (a * (mouseX - rect.left) + b * (mouseY - rect.top) + c * 0 + d) / (m * (mouseX - rect.left) + n * (mouseY - rect.top) + o * 0 + p);
+    const localY = (e * (mouseX - rect.left) + f * (mouseY - rect.top) + g * 0 + h) / (m * (mouseX - rect.left) + n * (mouseY - rect.top) + o * 0 + p);
+    
+    const xOffset = 450
+    const yOffset = -600
+    return { x: localX + xOffset, y: localY + yOffset};
   }
 
 }
