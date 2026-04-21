a1fba3a9ec7057f8ab0a02029cbe4a4c5a298e94
Nick Yeung <nickjyeung@gmail.com>
2026-04-21 13:28:40 -0500

    new updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, inject, ViewChild } from '@angular/core';
 import { RouterOutlet, Routes } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { Router } from '@angular/router'
 import { HomeComponent } from './home/home.component';
 import { CanvasComponent } from "./canvas/canvas.component";
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -13,7 +15,18 @@ import { CanvasComponent } from "./canvas/canvas.component";
 })
 export class AppComponent {
 
+  private updates = inject(SwUpdate)
+
   constructor(private router: Router) {}
+
+  ngOnInit() {
+    if (this.updates.isEnabled) {
+      this.updates.versionUpdates.subscribe(async ()=> {
+        await this.updates.activateUpdate();
+        document.location.reload()
+      })
+    }
+  }
 
   title = "Hi, I'm Nick";
 
